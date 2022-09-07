00da688728d4531c68477b474a6fa59bbc85cb75
Ahmet AKDEMIR <ahmet.akdemir@st.uskudar.edu.tr>
2022-09-07 11:23:18 +0300

    Add project files.

---
+++ CreationDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1, "ahmet");
+            Console.WriteLine(dictionary.Length);
+            foreach (var key in dictionary.Keys)
+            {
+                foreach (var value in dictionary.Values)
+                {
+                    Console.WriteLine(key);
+                    Console.WriteLine(value);
+
+                }
+            }
+        }
+    }
+}
